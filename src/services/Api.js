cc02702033733d47dfbf860c5db4a260cab5a980
david-borja <davidborjamartinez@gmail.com>
2021-07-24 16:47:49 +0200

    fix access to environment variables from api service

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,18 +1,18 @@
 export const requestAccessToken = async () => {
-  // I thought I could install dotenv and create an .env file to store these variables, but
-  // dotenv only works server-side.
+  // After my last commit where I couldn't access
+  // the variables from my .env file, I tried restarting the server, and it seems that worked.
 
-  const clientId = "notmyactualspotifyclientid";
-  const clientSecret = "notmyactualspotifyclientsecret";
-
-  // We could use webpack to create some sort of environment for dotenv to store the variables, as explained in this article (Method 2) https://trekinbami.medium.com/using-environment-variables-in-react-6b0a99d83cf5
-
+  // Now I can access what I have in the .env file through process.env but I don't really understand why it works, because:
+  // -I did not install dotenv
+  // -Dotenv is supposed to work only on the server, and here I'm accessing environment variables from the front
   const result = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
       // "Content-Type" must have quotes
       "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+      Authorization: `Basic ${btoa(
+        `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}:${process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}`
+      )}`,
       // "btoa" converts a binary data string to base64-encoded ASCII string. Authorization parameter must match this format for the Spotify API
     },
     body: "grant_type=client_credentials",
