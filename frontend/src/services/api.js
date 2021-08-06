380f07003a1141d35d47187824380db998d4e2ea
david-borja <davidborjamartinez@gmail.com>
2021-08-06 18:28:19 +0200

    add getUserPlaylists service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -19,3 +19,23 @@ export const search = async ({ qry, accessToken }) => {
     return error;
   }
 };
+
+export const getUserPlaylists = async ({ accessToken }) => {
+  try {
+    const result = await fetch(
+      `https://api.spotify.com/v1/me/playlists?limit=50`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const data = await result.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
